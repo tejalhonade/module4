d9d452c9c1f944a6adbf3d80768ba858378fbb90
tejalhonade <67832230+tejalhonade@users.noreply.github.com>
2020-08-06 22:25:13 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,8 +1,15 @@
+(function (window) {
+
+var helloSpeaker = {};
 
 var speakWord = "Hello";
 
 
-function speak(name) {
+helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
