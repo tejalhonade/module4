9c4fd32cab491b87181aabc7c31332ea4b9fc70d
tejalhonade <67832230+tejalhonade@users.noreply.github.com>
2020-08-06 22:22:18 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,9 +1,16 @@
+(function (window) {
 
-var speakWord = "Good Bye";
+var helloSpeaker = {};
+
+var speakWord = "Hello";
 
 
-function speak(name) {
+// See Lecture 52, part 2
+helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
 
+window.helloSpeaker = helloSpeaker;
+
+})(window);
