c9fa67e2be6cc473a121a1b44190fa553f8bbcc5
tejalhonade <67832230+tejalhonade@users.noreply.github.com>
2020-08-06 22:27:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
+(function (window) {
 
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
-
-(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
-for (var i = 0; i < names.length; i++) {
+for (var i in names) {
 
-  var firstLetter = names[i].charAt(0).toLowerCase();
+ 
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
 
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+  
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+     window.helloSpeaker.speak(names[i]);
   }
 }
-
-})();
+})(window);
